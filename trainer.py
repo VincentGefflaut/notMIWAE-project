14cb28bd84af1ca0c1484050b7661ae9c8c489e7
Vincent Gefflaut <vincent.gefflaut@gmail.com>
2025-11-27 10:54:59 +0100

    add losses plot

--- trainer.py
+++ trainer.py
@@ -10,6 +10,11 @@ def train(model, batch_size, max_iter=10000, name=None):
     start = time.time()
     best = float("inf")
 
+    # Track loss history
+    train_losses = []
+    val_losses = []
+    iterations = []
+
     for i in range(max_iter):
         loss = model.train_batch(batch_size=batch_size)
 
@@ -19,9 +24,16 @@ def train(model, batch_size, max_iter=10000, name=None):
 
             val_loss = model.val_batch()
 
+            # Store losses
+            train_losses.append(loss)
+            val_losses.append(val_loss)
+            iterations.append(i)
+
             if val_loss < best and name is not None:
                 best = val_loss
                 model.save(name)
             print("{0}/{1} updates, {2:.2f} s, {3:.2f} train_loss, {4:.2f} val_loss"
                   .format(i, max_iter, took, loss, val_loss))
             sys.stdout.flush()
+
+    return {'iterations': iterations, 'train_losses': train_losses, 'val_losses': val_losses}
